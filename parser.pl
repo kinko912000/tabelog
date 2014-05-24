dcd05fff7679db3332676f8adec305ef21f72b03
yoshiaki <kinko912000@gmail.com>
2014-05-25 01:20:34 +0900

    add scraping

--- parser.pl
+++ parser.pl
@@ -18,10 +18,12 @@ foreach my $a ($tokyo->look_down('id','arealst_sitemap')->find("a")) {
 
 #print @station_url;
 
+
+###駅ごとのあいうえお順にならんだリストのurlを取得
+
 my @station_url_aioue;
 my $station;
 
-###駅ごとのあいうえお順にならんだリストのurlを取得
 foreach my $a (@station_url) {
 	$station = parser($a);
 #	print $station->look_down('id','arealst_sitemap')->find("a")->attr('href');
@@ -31,13 +33,18 @@ foreach my $a (@station_url) {
 	}
 }
 
-my @station_url_aioue;
+
+
+
+
+
+###あいうえお順のurlリストから店のurlを取得
+
 my $restaurant;
 
 my $file  = "list.csv";
 open(my $fh,'>',$file) or die "Cannot open $file : $!";
 
-###あいうえお順のurlリストから店のurlを取得
 foreach my $a (@station_url_aioue) {
 	$restaurant = parser($a);
 #	print $station->look_down('id','arealst_sitemap')->find("a")->attr('href');
@@ -45,6 +52,40 @@ foreach my $a (@station_url_aioue) {
 	foreach my $b ($restaurant->look_down('id','rstlst_sitemap')->find("a")) {
     	print $fh ("http://tabelog.com/".$b->attr('href')."\n");
 	}
+}
+
+
+###ファイルからお店のurlを取得して内容を保存
+my $list  = "list.csv";
+open(my $fh_list,'<',$list) or die "Cannot open $list: $!";
+
+my $page_content;
+my $title;
+
+while (my $line = readline $fh_list){
+	chomp $line;
+
+	$page_content = scraping($line);
+	$title = (split(/\//,$line))[6];
+	open(my $fh_restaurant,'>',$title."\.txt") or die "Cannot open $list: $!";
+	print $fh_restaurant $page_content;
+
+}
+
+
+
+sub scraping {
+	my $url = shift;
+	# IE8のフリをする
+	my $user_agent = "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)";
+
+	# LWPを使ってサイトにアクセスし、HTMLの内容を取得する
+	my $ua = LWP::UserAgent->new('agent' => $user_agent);
+	my $res = $ua->get($url);
+	my $content = $res->content;
+
+	return $content;
+
 }
 
 
